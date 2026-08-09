40af43af7ef38b45584692e732e507eaaf90f2ce
Yogya Gamage <gamageyo@madagh.iro.umontreal.ca>
2026-08-08 22:48:01 -0400

    Add random path data

---
+++ src/test/java/org/apache/ibatis/logging/log4j2/Log4j2AbstractLoggerImplmethod_ExtendedLoggerWrappermethodFikaTest.java
@@ -0,0 +1,30 @@
+/*
+ *    Copyright 2009-2026 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.apache.ibatis.logging.log4j2;
+
+import org.apache.logging.log4j.spi.AbstractLogger;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+class Log4j2AbstractLoggerImplmethod_ExtendedLoggerWrappermethodFikaTest {
+
+  @Test
+  void testConstructorCallsExtendedLoggerWrapperConstructor() {
+    AbstractLogger mockLogger = Mockito.mock(AbstractLogger.class);
+    new Log4j2AbstractLoggerImpl(mockLogger);
+  }
+
+}
---
+++ src/test/java/org/apache/ibatis/logging/log4j2/Log4j2LoggerImpltrace_LoggertraceFikaTest.java
@@ -0,0 +1,31 @@
+/*
+ *    Copyright 2009-2026 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.apache.ibatis.logging.log4j2;
+
+import static org.mockito.Mockito.mock;
+
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Test;
+
+class Log4j2LoggerImpltrace_LoggertraceFikaTest {
+
+  @Test
+  void testTrace() {
+    Logger mockLogger = mock(Logger.class);
+    Log4j2LoggerImpl loggerImpl = new Log4j2LoggerImpl(mockLogger);
+    loggerImpl.trace("test message");
+  }
+}
